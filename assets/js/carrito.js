ff676cc00719e053dfb9b2a2d2729d9bbf3878e2
LeonelManjarres <leonel.manjarres.ttc.198@unilibre.edu.co>
2024-11-17 14:01:41 -0500

    import de productos a un nuevo js

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,13 +1,8 @@
+import{baseDedatos} from './productos.js';
+
 //DOM
 document.addEventListener('DOMContentLoaded', () => {
     // Variables - base de datos
-    const baseDedatos = [
-        { id: 1, nombre: 'Blusa Blanca', precio: 90000, imagen: 'assets/img/Damas/blusa1.jpg' },
-        { id: 2, nombre: 'Blusa Verde', precio: 110000, imagen: 'assets/img/Damas/blusa2.jpg' },
-        { id: 3, nombre: 'Buso Blanco&Azul', precio: 100000, imagen: 'assets/img/Caballero/buso1.jpg' },
-        { id: 4, nombre: 'Buso Vinotinto', precio: 130000, imagen: 'assets/img/Caballero/buso2.jpg' },
-        { id: 5, nombre: 'Gorra Negra', precio: 60000, imagen: 'assets/img/Accesorios/gorra1.jpg' }
-    ];
     
     let carrito = [];
     const divisa = ' Cop ';
---
+++ assets/js/productos.js
@@ -0,0 +1,8 @@
+// Lista de productos (base de datos)
+export const baseDedatos = [
+    { id: 1, nombre: 'Blusa Blanca', precio: 90000, imagen: 'assets/img/Damas/blusa1.jpg' },
+    { id: 2, nombre: 'Blusa Verde', precio: 110000, imagen: 'assets/img/Damas/blusa2.jpg' },
+    { id: 3, nombre: 'Buso Blanco&Azul', precio: 100000, imagen: 'assets/img/Caballero/buso1.jpg' },
+    { id: 4, nombre: 'Buso Vinotinto', precio: 130000, imagen: 'assets/img/Caballero/buso2.jpg' },
+    { id: 5, nombre: 'Gorra Negra', precio: 60000, imagen: 'assets/img/Accesorios/gorra1.jpg' }
+];
