355add4138f9b343e6fff82b780bdeec0c91f69f
LeonelManjarres <leonel.manjarres.ttc.198@unilibre.edu.co>
2024-11-17 14:54:35 -0500

    corregido caballero

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -2,7 +2,7 @@
 export const baseDedatos = [
     { id: 1, nombre: 'Blusa Blanca', precio: 90000, imagen: 'assets/img/Damas/blusa1.jpg' },
     { id: 2, nombre: 'Blusa Verde', precio: 110000, imagen: 'assets/img/Damas/blusa2.jpg' },
-    { id: 3, nombre: 'Buso Blanco&Azul', precio: 100000, imagen: 'assets/img/Caballero/buso1.jpg' },
-    { id: 4, nombre: 'Buso Vinotinto', precio: 130000, imagen: 'assets/img/Caballero/buso2.jpg' },
+    { id: 3, nombre: 'Buso Blanco&Azul', precio: 100000, imagen: 'assets/img/Caballero/Buso1.jpg' },
+    { id: 4, nombre: 'Buso Vinotinto', precio: 130000, imagen: 'assets/img/Caballero/Buso2.jpg' },
     { id: 5, nombre: 'Gorra Negra', precio: 60000, imagen: 'assets/img/Accesorios/gorra1.jpg' }
 ];
