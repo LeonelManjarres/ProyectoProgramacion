9652da78acc6cc594c02c0017f104af83ae35f6a
LeonelManjarres <leonel.manjarres.ttc.198@unilibre.edu.co>
2024-11-17 15:08:28 -0500

    agregando mas productos

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -3,6 +3,10 @@ export const baseDedatos = [
     { id: 1, nombre: 'Blusa Blanca', precio: 90000, imagen: 'assets/img/Damas/blusa1.jpg' },
     { id: 2, nombre: 'Blusa Verde', precio: 110000, imagen: 'assets/img/Damas/blusa2.jpg' },
     { id: 3, nombre: 'Buso Blanco&Azul', precio: 100000, imagen: 'assets/img/Caballero/Buso1.jpg' },
-    { id: 4, nombre: 'Buso Vinotinto', precio: 130000, imagen: 'assets/img/Caballero/Buso2.jpg' },
-    { id: 5, nombre: 'Gorra Negra', precio: 60000, imagen: 'assets/img/Accesorios/gorra1.jpg' }
+    { id: 4, nombre: ' Pantalon Caqui', precio: 130000, imagen: 'assets/img/Caballero/pantalon1.jpg'},
+    { id: 5, nombre: 'Bermuda jeans', precio: 95000, imagen: 'assets/img/Caballero/bermuda1.jpg'},
+    { id: 6, nombre: 'Buso Vinotinto', precio: 130000, imagen: 'assets/img/Caballero/Buso2.jpg' },
+    { id: 7, nombre: 'Gorra Negra', precio: 60000, imagen: 'assets/img/Accesorios/gorra1.jpg' },    
+    { id: 8, nombre: 'Gorra Azul', precio: 50000, imagen: 'assets/img/Accesorios/gorra2.jpg'},
+    
 ];
